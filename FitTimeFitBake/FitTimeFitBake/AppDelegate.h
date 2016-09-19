52f72cd2dcc5df0504f50644fcae74ab83231bdd
a5566baga <a5566baga@126.com>
2016-09-19 21:07:59 +0800

    初始化

---
+++ FitTimeFitBake/FitTimeFitBake/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FitTimeFitBake
+//
+//  Created by 张增强 on 16/9/19.
+//  Copyright © 2016年 张增强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
